2aba512072bca580cdbd5b2d75a85af289f24a9b
Rustam Musin <jvmusin@gmail.com>
2021-03-23 23:34:21 +0400

    Add Kotlin template

---
+++ content/contest/template.kt
@@ -0,0 +1,47 @@
+/**
+ * Author: Musin
+ * Source: Head
+ * Date: 2021-03-23
+ * Description: Template.
+ */
+
+const val YANDEX = false
+const val FORCE_LOCAL = false
+val OJ = !FORCE_LOCAL && (YANDEX || System.getProperty("ONLINE_JUDGE") != null)
+
+@Suppress("unused")
+val LOCAL = !OJ
+
+fun main() {
+    Locale.setDefault(Locale.US)
+    val fin = if (OJ) {
+//        val name: String? = "galaxy"
+//        System.setOut(PrintStream(BufferedOutputStream(FileOutputStream("$name.out"))))
+//        FastReader("$name.in")
+        System.setOut(PrintStream(BufferedOutputStream(System.out)))
+        FastReader()
+    } else {
+        FastReader()
+    }
+    Task(fin).solve()
+    System.out.flush()
+}
+
+class Task(private val fin: FastReader = FastReader()) {
+    fun solve() {
+    }
+}
+
+@Suppress("unused")
+class FastReader(fileName: String? = null) {
+    @JvmField
+    val br = BufferedReader(if (fileName != null) FileReader(fileName) else InputStreamReader(System.`in`))
+
+    @JvmField
+    var st = StringTokenizer("")
+
+    fun readString(): String {
+        while (!st.hasMoreTokens()) st = StringTokenizer(br.readLine())
+        return st.nextToken()
+    }
+}
